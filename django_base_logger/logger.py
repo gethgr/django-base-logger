698881705210edf26c991c55a830b949effba325
geth <altemode@mail.com>
2025-12-27 00:39:46 +0200

    add BaseLogger core console loggin

---
+++ django_base_logger/logger.py
@@ -0,0 +1,24 @@
+import logging
+
+class BaseLogger:
+    """Simple base logger for console output, reusable in Django projects."""
+    
+    def __init__(self, name: str):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.INFO)
+
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+    def info(self, msg: str):
+        self.logger.info(msg)
+
+    def warning(self, msg: str):
+        self.logger.warning(msg)
+
+    def error(self, msg: str):
+        self.logger.error(msg)
